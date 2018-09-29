611da8448cf0397a2ccd4715d07a4eb551881570
valery <valery.fremaux@gmail.com>
2018-09-29 12:15:21 +0200

    Fix GRDP

--- lang/fr/block_activity_publisher.php
+++ lang/fr/block_activity_publisher.php
@@ -1,7 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * @package   block_activity_publisher
+ * @copyright 2015 Valery Fremaux
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $string['activity_publisher:addinstance'] = 'Peut ajouter le bloc au cours';
 $string['activity_publisher:publish'] = 'Peut publier';
+
+// Privacy
+$string['privacy:metadata'] = 'Le bloc Publication d\'Activité ne détient directement aucune donnée relative aux utilisateurs.';
 
 $string['activity_instances'] = 'Instances';
 $string['activity_no_instances'] = 'Aucune instance pour cette ativité.';
